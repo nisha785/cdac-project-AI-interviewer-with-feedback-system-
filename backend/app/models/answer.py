4bb5a11b2e4f53eeb1a119e41179a19f4bb60934
Nisha Chandani <157677261+nisha785@users.noreply.github.com>
2026-07-30 20:23:04 +0530

    Delete backend directory

--- backend/app/models/answer.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import Column, Integer, Text, ForeignKey
-from .base import Base
-
-class Answer(Base):
-    __tablename__ = "answers"
-
-    answer_id = Column(Integer, primary_key=True, index=True)
-    question_id = Column(Integer, ForeignKey("questions.question_id"))
-    transcript = Column(Text)
